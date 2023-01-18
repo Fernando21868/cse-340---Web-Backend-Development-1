a38cb0dfdd9f9ec53039fb5e424ddd972d7bb2d9
FERNANDO <elderarias2015@gmail.com>
2023-01-18 01:15:45 -0300

    SQL && connections

--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -1,4 +1,4 @@
 <picture>
-    <img src="./images/site/logo.png" alt="Logo of the PHP Motors company, mainly black and blue." width="200" height="72">
+    <img src="/phpmotors/images/site/logo.png" width="200" height="72">
 </picture>
 <p>My Account</p>
---
+++ phpmotors/view/500.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template | PHP Motors</title>
+    <meta
+      name="description"
+      content="Template Page for Fernando Arias in CSE 340: Web Backend Development 1 at Brigham Young University - Idaho"
+  >
+    <link rel="stylesheet" href="../css/normalize.css" media="screen">
+    <link rel="stylesheet" href="../css/base.css" media="screen">
+    <link rel="stylesheet" href="../css/large.css" media="screen">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico?v=2">
+  </head>
+<body>
+  <div class="body">
+    <header>
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+    </header>
+    <nav>
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?>
+    </nav>
+    <main>
+        <h1>Server Error</h1>
+        <p class="error">Sorry our server seems to be experiencing some technical difficulties. Please check back later.</p>
+    </main>
+    <footer>
+      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+    </footer>
+  </div>
+</body>
+</html>
